3bb74b1c6d07e4803e322d55b05c0bd93089a1e3
Catherine Fish <katerinasheps@gmail.com>
2021-03-08 15:17:20 +0300

    New & Exit Buttons are resizeable
    - Added winfo_toplevel
    - Added grid configure

--- 03_ThreeWayAndTkinter/15_puzzle.py
+++ 03_ThreeWayAndTkinter/15_puzzle.py
@@ -2,16 +2,25 @@ import tkinter as tk
 
 class Application(tk.Frame):
     def __init__(self, master=None):
-        super().__init__(master)
-        self.grid()
+        tk.Frame.__init__(self, master)
+        self.grid(sticky=tk.N+tk.S+tk.E+tk.W)
         self.createWidgets()
 
     def createWidgets(self):
+        top = self.winfo_toplevel()                
+        top.rowconfigure(0, weight=1)
+        top.columnconfigure(0, weight=1)
+        
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        self.rowconfigure(0, weight=1)
+        
         self.newButton = tk.Button(self, text='New', command=self.new)
         self.exitButton = tk.Button(self, text='Exit', command=self.exit)
 
-        self.newButton.grid()
-        self.exitButton.grid()
+
+        self.newButton.grid(row=0, column=0, sticky="NEWS")
+        self.exitButton.grid(row=0, column=1, sticky="NEWS")
         
     def new(self):
         """Handler for the new button"""
