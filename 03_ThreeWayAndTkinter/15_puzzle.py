81dc6b36da8a9f978e7d26fb055a76d5f905efe0
Catherine Fish <katerinasheps@gmail.com>
2021-03-08 14:15:35 +0300

    Added new & exit buttons

--- 03_ThreeWayAndTkinter/15_puzzle.py
+++ 03_ThreeWayAndTkinter/15_puzzle.py
@@ -3,6 +3,25 @@ import tkinter as tk
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
+        self.grid()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.newButton = tk.Button(self, text='New', command=self.new)
+        self.exitButton = tk.Button(self, text='Exit', command=self.exit)
+
+        self.newButton.grid()
+        self.exitButton.grid()
+        
+    def new(self):
+        """Handler for the new button"""
+        pass
+
+    def exit(self):
+        """Handler for the exit button that closes the application window"""
+        pass
+   
+
         
 app = Application()
 app.master.title('15 PUZZLE')
