8cbc758174c898b2259cad07cb9a7f81ebe99ac7
Catherine Fish <katerinasheps@gmail.com>
2021-03-21 11:03:38 +0300

    Added char insert

--- 04_PublicRepositoryEvents/LabelEdit.py
+++ 04_PublicRepositoryEvents/LabelEdit.py
@@ -8,14 +8,14 @@ class Application(tk.Frame):
         '''Create root window with frame, tune weight and resize'''
         super().__init__(master, **kwargs)
         self.master.title(title)
-        #self.master.columnconfigure(0, weight=1)
-        #self.master.rowconfigure(0, weight=1)
+        self.master.columnconfigure(0, weight=1)
+        self.master.rowconfigure(0, weight=1)
         self.grid(sticky="NEWS")
         self.create_widgets()
-        #for column in range(self.grid_size()[0]):
-        #    self.columnconfigure(column, weight=1)
-        #for row in range(self.grid_size()[1]):
-        #    self.rowconfigure(row, weight=1)
+        for column in range(self.grid_size()[0]):
+            self.columnconfigure(column, weight=1)
+        for row in range(self.grid_size()[1]):
+            self.rowconfigure(row, weight=1)
 
     def create_widgets(self):
         '''Create all the widgets'''
@@ -23,13 +23,20 @@ class Application(tk.Frame):
 class InputLabel(tk.Label):
     def __init__(self, master=None, **kwargs):
         self.textField = tk.StringVar()
-        self.textField.set("My Label")
+        #self.textField.set("My Label")
         super().__init__(master, textvariable=self.textField, 
             takefocus=True, highlightthickness=3, highlightcolor='#000fff000')
         self.bind('<Button-1>', self.mouse_click)
+        self.bind('<KeyPress>', self.key_click)
+
 
     def mouse_click(self, event):
+        '''Mouse click handler'''
         self.focus()
+
+    def key_click(self, event):
+        '''Key click handler'''
+        self.textField.set(self.textField.get() + event.char)
 
 class App(Application):
     def create_widgets(self):
