f5abec466cc6f3dcb044f10edf4114ee7b7052c7
Catherine Fish <katerinasheps@gmail.com>
2021-03-27 18:40:04 +0300

    Added figures adding with text

--- 05_GraphEditor/task.py
+++ 05_GraphEditor/task.py
@@ -3,6 +3,7 @@ Tkinter skeleton app
 '''
 import tkinter as tk
 from itertools import product
+import re
 
 class Application(tk.Frame):
     '''Sample tkinter application class'''
@@ -34,11 +35,14 @@ class App(Application):
         self.canvas.bind("<Button-1>", self.justClickMouse)
         self.canvas.bind("<B1-Motion>", self.clickMouse)
         self.canvas.bind("<ButtonRelease-1>", self.releaseMouse)
+       
+        self.draw = tk.Button(self, text="Draw", command=self.draw) 
         self.Q = tk.Button(self, text="Quit", command=self.master.quit)
         
         self.text.grid(row=0, column=0, sticky="NEWS")
         self.canvas.grid(row=0, column=1, sticky="NEWS")
-        self.Q.grid(row=1, columnspan=2, sticky="NEWS")
+        self.draw.grid(row=1, column=0, sticky="NEWS")
+        self.Q.grid(row=1, column=1, sticky="NEWS")
 
     def justClickMouse(self, event):
         '''Only Mouse click handler'''
@@ -87,6 +91,16 @@ class App(Application):
                 self.text.insert(str(self.figureLog.index(self.cur_fig) + 1) + '.0', f"oval <{self.canvas.coords(self.cur_fig)[0]} {self.canvas.coords(self.cur_fig)[1]} {self.canvas.coords(self.cur_fig)[2]} {self.canvas.coords(self.cur_fig)[3]}> width={self.canvas.itemconfigure(self.cur_fig)['width'][-1]} outline={self.canvas.itemconfigure(self.cur_fig)['outline'][-1]} fill={self.canvas.itemconfigure(self.cur_fig)['fill'][-1]}\n")
             self.init_state = 0
 
+    def draw(self):
+        '''Draw button handler'''
+        for figure in self.canvas.find_all():
+            self.canvas.delete(figure)
+        self.figureLog.clear()
+        text = self.text.get('1.0', tk.END).split("\n")
+        for figure in text:
+            if len(figure):
+                coords, width, outline, fill = re.findall(r"oval <(.*)> width=(.*) outline=(.*) fill=(.*)", figure)[0]
+                self.figureLog.append(self.canvas.create_oval(coords.split(' '), width=width, outline=outline, fill=fill))
 
 app = App(title="Sample application")
 app.mainloop()
